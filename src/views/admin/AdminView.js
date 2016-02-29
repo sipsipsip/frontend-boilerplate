dd9bdecdc39c631b23a20149a1bc2c9ccbd2b253
Muhammad Azamuddin <azamuddin@live.com>
2016-02-29 19:18:36 +0700

    PATCH: admin filter

--- src/views/admin/AdminView.js
+++ src/views/admin/AdminView.js
@@ -3,6 +3,14 @@ import _ from 'lodash'
 import { connect } from 'react-redux'
 import * as actions from '../../redux/actions'
 
+class Unauthorized extends Component{
+    render(){
+        return <div style={{textAlign: 'center'}}>
+            <h2> 401<small>You're not authorized</small></h2>
+        </div>
+    }
+}
+
 class AdminView extends Component{
     constructor(props){
         super(props)
@@ -14,8 +22,13 @@ class AdminView extends Component{
     
     render(){
         let { currentUser } = this.props
-        return <div>
-                Welcome Admin {currentUser.nama} : ids = {currentUser.hasOwnProperty('role_ids') ? currentUser.role_ids[0] : 'Non Admin Leave!'}
+        let roles = currentUser.role_ids || [];
+
+        if(roles.indexOf(1) < 0){
+            return <Unauthorized/>
+        }
+        return <div style={{textAlign: 'center'}}>
+                <h2> Welcome <small> Administrator {currentUser.nama}</small></h2>
             </div>
     }
 }
